ec94eb6b33d154d201a817d7d2ac305f721d9504
Javier M. Ruzafa <javier.reyes@adidas.com>
2022-08-25 00:58:31 +0200

    Refactoring tests so they are clustered

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,10 +1,8 @@
 export function fizzBuzzOf(input: number) {
-  const fizz = 'Fizz';
-  const buzz = 'Buzz';
-  if (input % 3 == 0 && input % 5 == 0) return fizz+buzz;
+  const fizz = "Fizz";
+  const buzz = "Buzz";
+  if (input % 3 == 0 && input % 5 == 0) return fizz + buzz;
   if (input % 3 == 0) return fizz;
-  if(input % 5 == 0) return buzz;
-  if (input == 2) return "2";
-  if(input == 4) return '4';
-  return "1";
+  if (input % 5 == 0) return buzz;
+  return input.toString();
 }
--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -1,4 +1,4 @@
-import { fizzBuzzOf} from "../src/fizzBuzz";
+import { fizzBuzzOf } from "../src/fizzBuzz";
 /* Write a function that takes positive integers and outputs 
 their string representation.
 
@@ -12,7 +12,7 @@ Your function should comply with the following additional rules:
 
 -Otherwise it will return the number
 */
-// For example, given the numbers from 1 to 15 in order, 
+// For example, given the numbers from 1 to 15 in order,
 // the function would return:
 
 // 1
@@ -30,41 +30,39 @@ Your function should comply with the following additional rules:
 // 13
 // 14
 // FizzBuzz
-describe ("FizzBuzz Program", ()=>{
-   const fizz = 'Fizz';
-  const buzz = 'Buzz';
-    it ("should print a number as a string", ()=>{
-       expect(fizzBuzzOf(1)).toEqual('1');
-    })
-    it("should return 'Fizz' when passing 3", ()=>{
-      expect(fizzBuzzOf(3)).toEqual(fizz);
-    })
-    it("should return '2' when passing 2", ()=>{
-      expect(fizzBuzzOf(2)).toEqual('2');
-    })
-    it("should return '4' when passing 4", ()=>{
-      expect(fizzBuzzOf(4)).toEqual('4');
-    })
-    it("should return 'Buzz' when passing 5", ()=>{
-      expect(fizzBuzzOf(5)).toEqual(buzz);
-    })
-    //multiple of 3 => 'Fizz'
-    it("should return 'Fizz' when passing 6", ()=>{
-      expect(fizzBuzzOf(6)).toEqual(fizz);
-    })
-    it("should return 'Fizz' when passing 12", ()=>{
-      expect(fizzBuzzOf(12)).toEqual(fizz);
-    })
-    it("should return 'Fizz' when passing 18", ()=>{
-      expect(fizzBuzzOf(18)).toEqual(fizz);
-    })
-    //multiple of 5 => 'Buzz'
-    it("should return 'Buzz' when passing 10", ()=>{
-      expect(fizzBuzzOf(10)).toEqual(buzz);
-    })
-    //multiple of 3 and 5 should return 'FizzBuzz'
-    it("should return 'FizzBuzz' when passing 15", ()=>{
-      expect(fizzBuzzOf(15)).toEqual(fizz+buzz);
-    })
+describe("FizzBuzz Program", () => {
+  const fizz = "Fizz";
+  const buzz = "Buzz";
+  it("should print a number as a string", () => {
+    expect(fizzBuzzOf(1)).toEqual("1");
+  });
 
- });
+  it("should return '2' when passing 2", () => {
+    expect(fizzBuzzOf(2)).toEqual("2");
+  });
+  it("should return '4' when passing 4", () => {
+    expect(fizzBuzzOf(4)).toEqual("4");
+  });
+
+  //multiple of 3 => 'Fizz'
+  it("should return 'Fizz' when passing multiples of 3", () => {
+    expect(fizzBuzzOf(3)).toEqual(fizz);
+    expect(fizzBuzzOf(6)).toEqual(fizz);
+    expect(fizzBuzzOf(12)).toEqual(fizz);
+    expect(fizzBuzzOf(18)).toEqual(fizz);
+  });
+  //multiple of 5 => 'Buzz'
+  it("should return 'Buzz' when passing a multiple of 5", () => {
+    expect(fizzBuzzOf(5)).toEqual(buzz);
+    expect(fizzBuzzOf(10)).toEqual(buzz);
+  });
+  //multiple of 3 and 5 should return 'FizzBuzz'
+  it("should return 'FizzBuzz' when getting a multiple of 3 and 5", () => {
+    expect(fizzBuzzOf(15)).toEqual(fizz + buzz);
+    expect(fizzBuzzOf(30)).toEqual(fizz + buzz);
+  });
+
+  it("should return '11' when passing 11", () => {
+    expect(fizzBuzzOf(11)).toEqual("11");
+  });
+});
