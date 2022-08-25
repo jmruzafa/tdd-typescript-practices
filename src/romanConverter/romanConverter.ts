b69e885943a6eea129cdc1e76772dc6091f7d27c
Javier M. Ruzafa <javier.reyes@adidas.com>
2022-08-25 17:13:31 +0200

    Refactor after green tests

--- src/romanConverter/romanConverter.ts
+++ src/romanConverter/romanConverter.ts
@@ -1,6 +1,16 @@
+const symbols: Record<number, string> = { 0: "", 1: "I", 4: "IV", 5: "V" };
+
+const keys = Object.keys(symbols).map(key=> Number(key)).reverse();  
+
+function getKey(number: number): number {
+  return keys.filter(key => number >= key)[0];
+}
+
 export function romanConverter(num: number): string {
-  if (num >= 5) return "V" + romanConverter(num - 5);
-    if (num >= 4) return "IV" + romanConverter(num - 4);
-  if (num >= 1) return "I" + romanConverter(num - 1);
-  return "";
+    
+  const key = getKey(num);
+  if (key === 0) {
+    return symbols[0];
+  }
+  return symbols[key] + romanConverter(num - key);
 }
