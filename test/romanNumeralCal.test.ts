c1b3a8966699ae4aed97d2b0f249a81255ddf7ce
Javier M. Ruzafa <javier.reyes@adidas.com>
2022-08-25 16:34:35 +0200

    This a transformation process

--- test/romanNumeralCal.test.ts
+++ test/romanNumeralCal.test.ts
@@ -5,16 +5,21 @@ describe("Roman Numeral Calculator", () => {
     expect(romanConverter(0)).toEqual("");
   });
 
-  it("should return 'I ~ III' when getting 1~3", () => {
+  it("should return 'I~III' when getting 1~3", () => {
     expect(romanConverter(1)).toEqual("I");
     expect(romanConverter(2)).toEqual("II");
+    expect(romanConverter(3)).toEqual("III");
   });
+  it("should return 'IV' when getting 4", () => {
+    expect(romanConverter(4)).toEqual("IV");
+  });
+  
+  
 
-  it("should return 'V' when getting 5", () => {
+  /*  it("should return 'V' when getting 5", () => {
     expect(romanConverter(5)).toEqual("V");
   });
   it("should return 'X' when getting 10", () => {
     expect(romanConverter(10)).toEqual("X");
-  });
-
+  }); */
 });
